4f15040a247ec1ae964d75d993acda301adb73e7
Alexandr Egorov <egorov1011@gmail.com>
2026-04-21 14:48:19 +0300

    Done. M1 part2: task1,2,5 POST and DELETE methods

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from typing import Any
 from random import choice
 
@@ -55,8 +55,8 @@ app.route("/params/<value>")
 def param_example(value: str):
     return jsonify(param = value)
 
-@app.route("/quotes/<int:qute_id>")
-def get_quotes(qute_id:int)->dict:
+@app.route("/quotes/<int:quote_id>")
+def get_quotes_by_id(quote_id:int)->dict:
     for quote in   quotes:
         if quote["id"] == qute_id:
             return jsonify(quote), 200
@@ -69,6 +69,22 @@ def quotes_count():
 @app.route("/quotes/random", methods=["GET"])
 def random_quotes() -> dict:
     return jsonify(choice(quotes))
+
+@app.route("/quotes", methods=['POST'])
+def create_quote():
+    new_qoute = request.json
+    new_id = last_quote["id"] +1
+    new_qoute["id"] = new_id
+    quotes.append(new_qoute)
+    return jsonify(new_qoute), 201
+
+@app.route("/quotes/<int:quote_id>", methods=["DELETE"])
+def get_quotes_by_id(quote_id:int):
+    for quote in   quotes:
+        if quote["id"] == qute_id:
+            quotes.remove(quote)
+            return jsonify({"message": f"quote with id={quote_id} has deleted"}), 200
+    return {"error": f"quote with id= {qute_id} not found"}, 404
 
 
 if __name__ == "__main__":
